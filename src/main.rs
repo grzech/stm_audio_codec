1c04566e6d5b2de4aec6882188f53f18cf846afb
Grzegorz Czyrnek <grzegorz.czyrnek@gmail.com>
2023-07-26 19:35:21 +0200

    Remove tutorial code

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@
 
 use core::fmt::Write;
 
-// Imports
 use cortex_m_rt::entry;
 use panic_halt as _;
 use stm32f4xx_hal::{
-    gpio::Pin,
     pac::{self},
     prelude::*,
     serial::Config,
@@ -15,21 +13,16 @@ use stm32f4xx_hal::{
 
 #[entry]
 fn main() -> ! {
-    // Setup handler for device peripherals
     let dp = pac::Peripherals::take().unwrap();
-
-    // Configure the LED pin as a push pull ouput and obtain handler.
-    // On the Nucleo FR401 theres an on-board LED connected to pin PA5.
     let gpiod = dp.GPIOD.split();
-    let mut led = gpiod.pd15.into_push_pull_output();
-    let mut led2 = gpiod.pd14.into_push_pull_output();
-    let clocks = dp.RCC.constrain().cfgr.use_hse(8.MHz()).freeze();
-
-    // Configure the button pin (if needed) and obtain handler.
-    // On the Nucleo FR401 there is a button connected to pin PC13.
-    // Pin is input by default
     let gpioa = dp.GPIOA.split();
-    let button = gpioa.pa0;
+    let clocks = dp.RCC.constrain().cfgr.use_hse(8.MHz()).freeze();
+ 
+    let mut blue = gpiod.pd15.into_push_pull_output();
+    let mut red = gpiod.pd14.into_push_pull_output();
+    let mut orange = gpiod.pd13.into_push_pull_output();
+    let mut green = gpiod.pd12.into_push_pull_output();
+    let _button = gpioa.pa0;
 
     let usart_tx = gpioa.pa2.into_alternate::<7>();
     let usart_rx = gpioa.pa3.into_alternate::<7>();
@@ -41,42 +34,17 @@ fn main() -> ! {
                                                      &clocks)
                                         .unwrap();
     
-    // Create and initialize a delay variable to manage delay loop
-    let mut del_var = 10_0000_u32;
-
-    // Initialize LED to on or off
-    led.set_low();
-    led2.set_high();
-    // Application Loop
-    loop {
-        // Call delay function and update delay variable once done
-        del_var = loop_delay(del_var, &button);
-
-        // Toggle LED
-        led.toggle();
-        led2.toggle();
-        writeln!(&mut usart, "Some text to send").unwrap();
-
+    blue.set_high();
+    red.set_high();
+    orange.set_high();
+    green.set_high();
+    writeln!(&mut usart, "Peripherals are initialized").unwrap();
+    blue.set_low();
+    red.set_low();
+    orange.set_low();
+    green.set_low();
+    writeln!(&mut usart, "Entering mainloop").unwrap();
+    loop {        
+        blue.toggle();
     }
 }
-
-// Delay Function
-fn loop_delay<const P: char, const N: u8>(mut del: u32, but: &Pin<P, N>) -> u32 {
-    // Loop for until value of del
-    for _i in 1..del {
-        // Check if button got pressed
-        if but.is_low() {
-            // If button pressed decrease the delay value
-            del = del - 2_5000_u32;
-            // If updated delay value reaches zero then reset it back to starting value
-            if del < 2_5000 {
-                del = 10_0000_u32;
-            }
-            // Exit function returning updated delay value if button pressed
-            return del;
-        }
-    }
-    // Exit function returning original delay value if button no pressed (for loop ending naturally)
-    del
-}
-
